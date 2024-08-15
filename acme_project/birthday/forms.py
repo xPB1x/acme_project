44db47746bda84edfd532172ea2d2f4d47beb448
Pavel Bryzgalov <bryzgalovpaul@yandex.ru>
2024-08-15 20:06:17 +0300

    first_lesson

---
+++ acme_project/birthday/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+
+
+class BirthdayForm(forms.Form):
+    first_name = forms.CharField(
+        max_length=20,
+        label='Имя',
+    )
+    last_name = forms.CharField(
+        required=False,
+        label='Фамилия',
+        help_text='Необязательное поле'
+    )
+    birthday = forms.DateField(
+        label='Дата рождения',
+        widget=forms.DateInput(attrs={'type': 'date'})
+    )
--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 
+from .forms import BirthdayForm
+
 
 def birthday(request):
-    context = {}
-    return render(request, 'birthday/birthday.html', context=context)
+    form = BirthdayForm
+    context = {
+        'form': form
+    }
+    return render(request, 'birthday/birthday.html', context)
